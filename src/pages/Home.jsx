3ebb56a414b4daa16d35340d90672a14b3b5ef22
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-02-18 06:17:26 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,24 @@
-import Announcement from '../components/Announcement';
-import Navbar from '../components/Navbar';
-import Slider from '../components/Slider';
+import React from "react";
+import Announcement from "../components/Announcement";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+import Slider from "../components/Slider";
 
 const Home = () => {
-	return (
-		<>
-			<Announcement />
-			<Navbar />
-			<Slider />
-		</>
-	);
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products/>
+      <Newsletter/>
+      <Footer/>
+    </div>
+  );
 };
+
 export default Home;
